5d8ef0a5ccf76612f33d085991ae47b230278b01
noahmancino <noahdanielmancino@gmail.com>
2020-10-17 23:25:07 -0700

    Quick fix! Ownership is a pretty cool idea and the compiler is pretty helpful.

--- src/state.rs
+++ src/state.rs
@@ -1,26 +1,6 @@
 /*
-    This file defines the structure of the systems memory state.
+    This module defines the structure of the systems memory state.
  */
-
-
-#[allow(dead_code)]
-pub struct RegisterFile {
-    rax: i64,
-    rcx: i64,
-    rdx: i64,
-    rbx: i64,
-    rsp: i64,
-    rbp: i64,
-    rsi: i64,
-    rdi: i64,
-    r8: i64,
-    r9: i64,
-    r10: i64,
-    r11: i64,
-    r12: i64,
-    r13: i64,
-    r14: i64
-}
 
 // "Processor" status code
 #[allow(dead_code)]
